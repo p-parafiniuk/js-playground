b5dd3d28f6b15ec87bebaae85d979de384fec0a9
p-parafiniuk <pawel.parafiniuk@netguru.com>
2023-12-22 14:02:58 +0100

    Add checkbox and index number to list items

--- packages/ui/src/list-dnd/ListDnd.tsx
+++ packages/ui/src/list-dnd/ListDnd.tsx
@@ -1,19 +1,23 @@
 import cx from 'clsx';
-import { Text } from '@mantine/core';
+import { Text, Checkbox } from '@mantine/core';
 import { useListState } from '@mantine/hooks';
 import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd';
 import classes from './ListDnd.module.css';
 
 const data = [
-  { position: 6, mass: 12.011, symbol: 'C', name: 'Carbon' },
-  { position: 7, mass: 14.007, symbol: 'N', name: 'Nitrogen' },
-  { position: 39, mass: 88.906, symbol: 'Y', name: 'Yttrium' },
-  { position: 56, mass: 137.33, symbol: 'Ba', name: 'Barium' },
-  { position: 58, mass: 140.12, symbol: 'Ce', name: 'Cerium' },
+  { checked: false, position: 6, mass: 12.011, symbol: 'C', name: 'Carbon' },
+  { checked: true, position: 7, mass: 14.007, symbol: 'N', name: 'Nitrogen' },
+  // { position: 39, mass: 88.906, symbol: 'Y', name: 'Yttrium' },
+  // { position: 56, mass: 137.33, symbol: 'Ba', name: 'Barium' },
+  // { position: 58, mass: 140.12, symbol: 'Ce', name: 'Cerium' },
 ];
 
 export function ListDnd() {
   const [state, handlers] = useListState(data);
+
+  const allChecked = state.every((value) => value.checked);
+  const indeterminate = state.some((value) => value.checked) && !allChecked;
+  // mt="xs"
 
   const items = state.map((item, index) => (
     <Draggable key={item.symbol} index={index} draggableId={item.symbol}>
@@ -24,7 +28,20 @@ export function ListDnd() {
           {...provided.dragHandleProps}
           ref={provided.innerRef}
         >
-          <Text className={classes.symbol}>{item.symbol}</Text>
+          <Text className={classes.symbol}>{index+1}.&nbsp;</Text>
+
+          <Checkbox
+            key={item.name}
+            checked={item.checked}
+            onChange={(event) => handlers.setItemProp(index, 'checked', event.currentTarget.checked)}
+
+            label={
+              <>
+                {/* {index}.&nbsp; */}
+              </>
+            }
+          />
+
           <div>
             <Text>{item.name}</Text>
             <Text c="dimmed" size="sm">
